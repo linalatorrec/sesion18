f7a3ccecbaa603566f364041e1653da09d9ef46e
linalatorrec <lina.latorre.ttc.576@unilibre.edu.co>
2024-11-19 20:04:47 -0500

    Punto1

---
+++ punto1.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre: ")
+edad = int (input("Ingrese su edad: "))
+estatura = float(input("Ingrese su estatura (en metros):"))
+estudiante = input("Eres estudiante (si/no): ").scrip().lower()=="si"
+
+print("Hola, mi nombre es: ", nombre, "Mi edad es: ", edad, "Mido: ", estatura, "Estudio: ", estudiante)
